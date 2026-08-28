1a81e589d974684737e338b5cb5576d42b4b84e5
Burhanverse <contact@burhanverse.eu.org>
2026-08-28 20:49:14 +0530

    chore: bump version to 1.5.0
    Signed-off-by: Burhanverse <contact@burhanverse.eu.org>

--- src/version.hpp
+++ src/version.hpp
@@ -5,10 +5,10 @@
 namespace webclip {
 
 inline constexpr int VERSION_MAJOR = 1;
-inline constexpr int VERSION_MINOR = 4;
-inline constexpr int VERSION_PATCH = 2;
+inline constexpr int VERSION_MINOR = 5;
+inline constexpr int VERSION_PATCH = 0;
 
-inline constexpr std::string_view VERSION_STRING = "1.4.2";
+inline constexpr std::string_view VERSION_STRING = "1.5.0";
 inline constexpr std::string_view APP_NAME = "WebClip";
 inline constexpr std::string_view APP_DISPLAY_NAME = "WebClip Sync";
 inline constexpr std::string_view APP_ORGANIZATION = "Burhanverse";
